400baa1d33e024999e04dd45f3bf591fcab34313
alexMercer94 <alex.c.valera94@hotmail.com>
2020-06-05 16:23:34 -0500

    Last details

--- components/ui/Search.js
+++ components/ui/Search.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
+import Router from 'next/router';
 
 const InputText = styled.input`
     border: 1px solid var(--gris3);
@@ -28,13 +29,33 @@ const InputSumit = styled.button`
 `;
 
 const Search = () => {
+    const [search, setSearch] = useState('');
+
+    const searchProduct = (e) => {
+        e.preventDefault();
+
+        if (search.trim() === '') return;
+
+        Router.push({
+            pathname: '/search',
+            query: {
+                q: search,
+            },
+        });
+    };
+
     return (
         <form
             css={css`
                 position: relative;
             `}
+            onSubmit={searchProduct}
         >
-            <InputText type="text" placeholder="Buscar Productos"></InputText>
+            <InputText
+                type="text"
+                placeholder="Buscar Productos"
+                onChange={(e) => setSearch(e.target.value)}
+            ></InputText>
             <InputSumit type="submit">Buscar</InputSumit>
         </form>
     );
--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout';
+import { useRouter } from 'next/router';
+import ProductDetails from '../components/layout/ProductDetails';
+import useProducts from '../hooks/useProducts';
 
 const Search = () => {
+    const router = useRouter();
+    const {
+        query: { q },
+    } = router;
+
+    const { products } = useProducts('createdAt');
+    const [result, setResult] = useState([]);
+
+    useEffect(() => {
+        if (q !== undefined) {
+            const search = q.toLowerCase();
+            const resultFilter = products.filter((product) => {
+                return (
+                    product.name.toLowerCase().includes(search) || product.description.toLowerCase().includes(search)
+                );
+            });
+
+            setResult(resultFilter);
+        }
+    }, [q, products]);
+
     return (
         <div>
             <Layout>
-                <h1>Buscar</h1>
+                <div className="listado-productos">
+                    <div className="contenedor">
+                        <ul className="bg-white">
+                            {result.map((product) => (
+                                <ProductDetails key={product.key} product={product}></ProductDetails>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </Layout>
         </div>
     );
